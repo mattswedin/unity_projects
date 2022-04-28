8c9fa4067cc7cf25dc6993e407f581eadcaa64d2
mattswedin <swedin.matthew@gmail.com>
2022-04-28 16:53:59 -0400

    implemetn playerController

---
+++ SnowBoarderJulian/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField] float torqueAmount = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb2d.AddTorque(torqueAmount);
+        } else if (Input.GetKey(KeyCode.RightArrow)) 
+        {
+           rb2d.AddTorque(-torqueAmount); 
+        }
+    }
+}
