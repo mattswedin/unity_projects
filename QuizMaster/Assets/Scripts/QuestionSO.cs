69d0f0de767041f7258be70c2f7a60b0c4278770
mattswedin <swedin.matthew@gmail.com>
2022-05-10 19:13:48 -0400

    implmenet getter method GetQuestion()

--- QuizMaster/Assets/Scripts/QuestionSO.cs
+++ QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -8,4 +8,9 @@ public class QuestionSO : ScriptableObject
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
 
+    public string GetQuestion()
+    {
+        return question;
+    }
+
 }
