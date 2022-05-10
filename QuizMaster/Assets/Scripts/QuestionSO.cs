83f28abc4e973c03c902a06f75273d85c7fc88da
mattswedin <swedin.matthew@gmail.com>
2022-05-09 21:46:44 -0400

    implement scriptable objects

---
+++ QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question text here";
+
+}
