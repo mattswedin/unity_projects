5c52ac77b18ac7089d1784993e24cc93d74317c7
mattswedin <swedin.matthew@gmail.com>
2022-05-11 09:47:51 -0400

    add using TMPro

--- QuizMaster/Assets/Scripts/QuestionSO.cs
+++ QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -7,10 +7,22 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int indexOfCorrectAnswer = 1;
 
     public string GetQuestion()
     {
         return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return indexOfCorrectAnswer;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
     }
 
 }
---
+++ QuizMaster/Assets/Scripts/Quiz.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Quiz : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI questionText;
+    [SerializeField] QuestionSO question;
+
+    void Start()
+    {
+        questionText.text = question.GetQuestion();
+    }
+
+}
