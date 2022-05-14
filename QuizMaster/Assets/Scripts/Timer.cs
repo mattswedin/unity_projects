0123784993220690f4b1a49e59aa0dc102d73c41
mattswedin <swedin.matthew@gmail.com>
2022-05-14 11:40:49 -0400

    implement timer

--- QuizMaster/Assets/Scripts/Timer.cs
+++ QuizMaster/Assets/Scripts/Timer.cs
@@ -32,7 +32,7 @@ public class Timer : MonoBehaviour
             if(timerValue <= 0)
             {
                 isAnsweringQuestion = true;
-                timerValue = timeToCompleteQuestion;
+                timerValue = timeToCompleteQuestion; 
             }
         }
 
