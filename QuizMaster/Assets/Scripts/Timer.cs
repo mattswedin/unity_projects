6e72696c623270136e7587fdae9c3f6428578308
mattswedin <swedin.matthew@gmail.com>
2022-05-13 22:44:13 -0400

    implement timer

---
+++ QuizMaster/Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float timerValue;
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+
+    public bool isAnsweringQuestion = false;
+
+    void Update()
+    {
+        UdateTimer();
+    }
+
+    void UdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if(isAnsweringQuestion)
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;  
+            }
+        }
+        else
+        {
+            if(timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+
+        Debug.Log(timerValue);
+    }
+}
