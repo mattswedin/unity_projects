5987aab97b7f2753e2c2d649e92f6aa4ee96cdbf
mattswedin <swedin.matthew@gmail.com>
2022-05-25 19:20:15 -0400

    implement input management && player movement

---
+++ TileVania/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+    [SerializeField] float playerSpeed = 3f;
+   
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2 (moveInput.x * playerSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+}
