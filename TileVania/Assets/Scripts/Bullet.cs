4c8a05ea9b610132c7c3a085e6d5f4f25169b9eb
mattswedin <swedin.matthew@gmail.com>
2022-06-07 10:53:54 -0400

    implement bullets, destroy enemy, and levels

--- TileVania/Assets/Scripts/Bullet.cs
+++ TileVania/Assets/Scripts/Bullet.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField] float bulletSpeed = 20f;
+    PlayerMovement player;
+    Rigidbody2D myRigidbody;
+    float xSpeed;
+    
+
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>();
+        myRigidbody = GetComponent<Rigidbody2D>();
+        xSpeed = player.transform.localScale.x * bulletSpeed;
     }
 
     void Update()
     {
-        
+        myRigidbody.velocity = new Vector2 (xSpeed, 0f);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);  
+        }
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Destroy(gameObject);
     }
 }
