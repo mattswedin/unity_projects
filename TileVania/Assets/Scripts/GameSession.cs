1f8f24f518e42c48f8d314ff315811fea2cc6b4f
mattswedin <swedin.matthew@gmail.com>
2022-06-13 12:07:23 -0400

    implement GameSession

--- TileVania/Assets/Scripts/GameSession.cs
+++ TileVania/Assets/Scripts/GameSession.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameSession : MonoBehaviour
 {
-    void Start()
+
+    [SerializeField] int playerLives = 3;
+
+    void Awake()
     {
-        
+        int numGameSessions = FindObjectsOfType<GameSession>().Length;
+        if (numGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
-    void Update()
+    public void ProcessPlayerDeath()
+    {
+        if (playerLives > 1)
+        {
+            // TakeLife();
+        }
+        else
+        {
+            ResetGameSession();
+        }
+    }
+
+    void ResetGameSession()
     {
         
     }
