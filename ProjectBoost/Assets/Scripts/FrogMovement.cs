a0f29d297d54f670b2dc01917a26ee30397bde03
mattswedin <swedin.matthew@gmail.com>
2022-08-31 02:02:59 -0400

    implement frog Move

--- ProjectBoost/Assets/Scripts/FrogMovement.cs
+++ ProjectBoost/Assets/Scripts/FrogMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FrogMovement : MonoBehaviour
 {
+    [Header("Frog")]
     [SerializeField] float minTimeBetweenJumps = .1f;
     [SerializeField] float maxTimeBetweenJumps = 1f;
     [SerializeField] float jumpHeight = 15f;
@@ -11,10 +12,19 @@ public class FrogMovement : MonoBehaviour
     [SerializeField] bool canJumpAgain = false;
     [SerializeField] AudioClip jumpSFX;
     [SerializeField] AudioClip ribbet;
+    [Header("Moving Frog")]
+    [SerializeField] bool isMoving;
+    [SerializeField] float speed = 5f;
+    [SerializeField] Transform[] wayPoints;
+    [SerializeField] Transform[] rotation;
+    [SerializeField] float rotationSpeed = 20f;
+    [SerializeField] float extraHeight = 2.5f;
     [Header("Score Frog")]
     [SerializeField] bool isScoreFrogRunning;
     [SerializeField] bool isScoreFrogEndPosition;
     [SerializeField] float spinSpeed = 5f;
+    int i = 0;
+    int j = 0;
 
     Rigidbody rb;
     AudioSource audioSource;
@@ -31,6 +41,10 @@ public class FrogMovement : MonoBehaviour
         if (!isScoreFrogRunning && !isScoreFrogEndPosition)
         {
             FrogJump();
+            if(isMoving)
+            {
+                Move();
+            }
         }
         else if (isScoreFrogRunning)
         {
@@ -48,6 +62,40 @@ public class FrogMovement : MonoBehaviour
             rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
         }
     
+    }
+
+    void Move()
+    {
+        if (i < wayPoints.Length)
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                                                    wayPoints[i].position,
+                                                    speed * Time.deltaTime);
+            if (transform.position == wayPoints[i].position)
+            {
+                
+                transform.rotation = Quaternion.RotateTowards(transform.rotation,
+                                                    rotation[j].rotation,
+                                                    rotationSpeed * Time.deltaTime);
+                
+                if (transform.rotation == rotation[j].rotation)
+                {
+                    rb.AddForce(Vector3.up * jumpHeight * extraHeight, ForceMode.Impulse);
+                    j++;
+                    i++;
+                    
+                    if (j == rotation.Length)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+            if (i == wayPoints.Length)
+            {
+                i = 0;
+            }
+        }
     }
 
     void OnCollisionEnter(Collision other) {
