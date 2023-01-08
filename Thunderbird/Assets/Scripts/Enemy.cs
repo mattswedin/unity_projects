5963ee68949d0639939d138151462f7b83189b91
mattswedin <swedin.matthew@gmail.com>
2023-01-08 12:05:20 -0500

    implement enemy flash when lasered

--- Thunderbird/Assets/Scripts/Enemy.cs
+++ Thunderbird/Assets/Scripts/Enemy.cs
@@ -6,6 +6,7 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 100;
     [SerializeField] Material[] hitColor;
+    int hitColorIndex = 0;
     PlayerController playerController;
     List<Material> og = new List<Material>();
     MeshRenderer[] meshRenderers;
@@ -24,15 +25,24 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other) 
     {
-        int rando = Random.Range(0, hitColor.Length); 
+        
+
         for (int i = 0; i < meshRenderers.Length; i++)
         {
             MeshRenderer mesh = meshRenderers[i];
             og.Add(mesh.material);
-            mesh.material = hitColor[rando];
+            mesh.material = hitColor[hitColorIndex];
         }
         if (!ogObtained) ogObtained = true;
         health -= playerController.GetLaserPower();
+        if (hitColorIndex == 0)
+        {
+            hitColorIndex = 1;
+        }
+        else
+        {
+            hitColorIndex = 0;
+        }
         if (health < 1) Destroy(gameObject);
     }
 
