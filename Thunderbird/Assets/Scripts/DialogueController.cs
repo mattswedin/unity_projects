d54480696d5a2af5adbd7e93559f7f5ffe830f13
mattswedin <swedin.matthew@gmail.com>
2023-04-08 00:49:20 -0400

    implement cutscene

--- Thunderbird/Assets/Scripts/DialogueController.cs
+++ Thunderbird/Assets/Scripts/DialogueController.cs
@@ -6,12 +6,14 @@ using UnityEngine.UI;
 
 public class DialogueController : MonoBehaviour
 {
-
+    [SerializeField] GameObject dialogueObj;
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] string[] dialogue;
     [SerializeField] bool isSpeaking;
     [SerializeField] int dialogueIndex;
     [SerializeField] float timeBetweenChars = .1f;
+    [SerializeField] float defaultTimeBetweenChars = .1f;
+    [SerializeField] GameObject nextScene;
 
     PlayerController playerController;
 
@@ -22,22 +24,47 @@ public class DialogueController : MonoBehaviour
 
     void Start()
     {
+        dialogueObj.SetActive(true);
         playerController.SetCantMove(true);
+        StartCoroutine(PlayDialogue());
+    }
+
+    void Update() 
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (!isSpeaking)
+            {
+                dialogueIndex++;
+                StartCoroutine(PlayDialogue());
+            }
+        }
     }
 
     IEnumerator PlayDialogue() 
     {
-        isSpeaking = true;
         
-        for (int i = 0; i < dialogue[dialogueIndex].Length; i++)
+        text.text = "";
+
+        if (dialogueIndex < dialogue.Length)
         {
-            char letter = dialogue[dialogueIndex][i];
-            text.text += letter;
-            yield return new WaitForSeconds(timeBetweenChars);
+            isSpeaking = true;
+
+            for (int i = 0; i < dialogue[dialogueIndex].Length; i++)
+            {
+                char letter = dialogue[dialogueIndex][i];
+                text.text += letter;
+                if (letter != ' ') yield return new WaitForSeconds(timeBetweenChars);
+            }
+
+            isSpeaking = false;
+        }
+        else
+        {
+            nextScene.SetActive(true);
         }
 
-        dialogueIndex++;
-
+       
     }
 
    
